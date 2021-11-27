988b3946be74e186fb03d1a6ced4bc4ed505a4de
unknown <ghaliarose89@gmail.com>
2021-11-27 13:59:44 -0600

    adding posts to h.p

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -65,7 +65,6 @@ router.post('/', (req, res) => {
         req.session.user_id = dbUserData.id;
         req.session.username = dbUserData.username;
         req.session.loggedIn = true;
-
         res.json(dbUserData);
       });
     })
@@ -96,7 +95,7 @@ router.post('/login', (req, res) => {
 
     req.session.save(() => {
       req.session.user_id = dbUserData.id;
-      req.session.username = dbUserData.username;
+      req.session.email = dbUserData.email;
       req.session.loggedIn = true;
 
       res.json({ user: dbUserData, message: 'You are now logged in!' });
